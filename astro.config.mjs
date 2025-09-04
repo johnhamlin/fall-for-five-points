c4843eb512968de34b87584aafd3110d6c6d7e7c
John Hamlin <johnhamlin@gmail.com>
2025-09-04 17:05:26 -0400

    Run prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare({
     platformProxy: {
-      enabled: true
+      enabled: true,
     },
 
-    imageService: "cloudflare"
-  })
+    imageService: "cloudflare",
+  }),
 });
